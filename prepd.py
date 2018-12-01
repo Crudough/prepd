66dc035674e208cd897ad4a4354b6fb69ddc09b1
Anthony Crudo <anthony.crudo@queensu.ca>
2018-12-01 14:04:42 -0500

    Inclusion of Jinja Templating
    Includes making the front end more modular.

--- prepd.py
+++ prepd.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def prepd():
 	return render_template('home.html')
 
 @app.route('/the-pass')
