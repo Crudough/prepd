f15576c835a088f94c1a0fad0b0374b5cb56ba4b
Anthony Crudo <anthony.crudo@queensu.ca>
2018-12-01 15:06:34 -0500

    WIP of Flyer.html

--- prepd.py
+++ prepd.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for
+import prepd_tools
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +10,26 @@ def prepd():
 @app.route('/the-pass')
 def thePass():
 	return render_template('pass.html')
+
+@app.route('/metro-flyer')
+def metro():
+	store = 'Metro'
+	json = prepd_tools.get_JSON('english', 'metro', 'K7L4A5')
+	images = prepd_tools.get_img(json)
+	names = prepd_tools.get_name(json)
+	return render_template('flyer.html', store=store, images=images, names=names)
+
+@app.route('/superstore-flyer')
+def superstore():
+	store = 'Great Canadian Superstore'
+	return render_template('flyer.html', store=store, images=images)
+
+@app.route('/no-frills-flyer')
+def no_frills():
+	store = 'No Frills'
+	return render_template('flyer.html', store=store, images=images)
+
+@app.route('/sobeys-flyer')
+def sobeys():
+	store = 'Sobeys'
+	return render_template('flyer.html', store=store, images=images)
