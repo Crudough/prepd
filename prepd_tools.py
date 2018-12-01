0b2fc101c4b0cfeeb93eff10e0e28010c5832278
Evan Bonneau <bonneauevan@gmail.com>
2018-12-01 13:57:30 -0500

    adding getjson and getimg functions

--- prepd_tools.py
+++ prepd_tools.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env python
+import urllib.request as req
+import json 
 
+
+def get_JSON(locale, store, code):
+    #print(STORE, POSTAL)
+    base_url = "https://backflipp.wishabi.com/flipp/items/search?"
+    url = base_url + "locale=" + locale + "&postal_code=" + str(code) + "&q=" + store
+    #print(url)
+    flyer = req.urlopen(url)
+    #print(flyer.read())
+    return flyer.read()        
+            
+
+
+
+def get_IMG(JSON):
+    for arg in JSON["items"]: 
+        print(arg["clipping_image_url"])
+
+'''
+print("-------------------------- starting script ------------------------------------")
+code = "V9L6A8"         #input("please enter postal code: ")
+store = "superstore"    #input("please enter prefered store: ")
+locale = "francais"     #input("please enter locale: ")
+b = get_JSON(locale, store, code)
+#print (b)
+flyer_JSON = json.loads(b)
+
+get_IMG(flyer_JSON)
+
+print("--------------------------- ending  script ------------------------------------")
+'''
 
